23ef47cb84b2e9285f9688774cad65cfb9833486
Alex Dilley <alex.dilley@gmail.com>
2020-02-09 14:00:24 +0000

    fix: livereload - watch `dist` dir

--- rollup.config.js
+++ rollup.config.js
@@ -70,9 +70,9 @@ export default {
     // In dev mode, call `npm run start` once the bundle has been generated.
     !production && serve(),
 
-    // Watch the `public` directory and refresh the browser on changes when not
-    // in production.
-    !production && livereload('public'),
+    // Watch the `dist` directory and refresh the browser on changes when not in
+    // production.
+    !production && livereload('dist'),
 
     // If we're building for production (npm run build instead of npm run dev),
     // minify.
