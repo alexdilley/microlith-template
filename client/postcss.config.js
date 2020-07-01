9124e6d9ae48fa1d3a2fc2f9eb81b1260a105b3c
Alex Dilley <alex.dilley@gmail.com>
2020-07-01 11:44:09 +0100

    extend postcss featureset

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,6 +2,11 @@ module.exports = (ctx) => ({
   plugins: {
     'postcss-import': true,
     tailwindcss: true,
+    'postcss-preset-env': {
+      features: {
+        'nesting-rules': true,
+      },
+    },
     cssnano: ctx.env === 'production' ? {} : false,
   },
 });
