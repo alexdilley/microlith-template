222cc2002003632dee67083e313ba3c607a687a5
Alex Dilley <alex.dilley@gmail.com>
2020-07-01 11:41:08 +0100

    svelte'y eslint accommodations

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -7,11 +7,20 @@ module.exports = {
     browser: true,
   },
   plugins: ['svelte3'],
+  rules: {
+    // Dependencies have dev scope ∵ compiler.
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
   overrides: [
     {
       files: ['*.svelte'],
       processor: 'svelte3/svelte3',
+      settings: {
+        // Avoid pre-processor fisticuffs.
+        'svelte3/ignore-styles': () => true,
+      },
       rules: {
+        'import/first': 'off', // imports occur within `<script>` tags
         'import/no-mutable-exports': 'off', // props
       },
     },
