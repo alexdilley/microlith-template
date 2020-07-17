a3e78c87ef60bfc314f6b4a0ee686439877f1f26
Alex Dilley <alex.dilley@gmail.com>
2020-07-17 10:45:46 +0100

    expose dev instance over network by default

--- client/bin/serve.js
+++ client/bin/serve.js
@@ -4,16 +4,21 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const polka = require('polka');
 const sirv = require('sirv');
 
-const { API_BASE_URL, PORT = 3000, ROLLUP_WATCH } = process.env;
+const {
+  API_BASE_URL,
+  HOST = '0.0.0.0',
+  PORT = 3000,
+  ROLLUP_WATCH,
+} = process.env;
 const dev = !!ROLLUP_WATCH;
 
 polka()
   .use(
     createProxyMiddleware('/api', { target: API_BASE_URL, changeOrigin: true }),
-    sirv('dist', { dev, single: true })
+    sirv('dist', { single: true, host: true, dev })
   )
-  .listen(PORT, (err) => {
+  .listen(PORT, HOST, (err) => {
     if (err) throw err;
     // eslint-disable-next-line no-console
-    console.log(`> Ready on localhost:${PORT}~!`);
+    console.log('Your application is ready~! 🚀');
   });
